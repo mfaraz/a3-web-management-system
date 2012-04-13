f57fa27e58030b8b6767129e0bbbe4d8b9a4ffb2
dhiauddin <dhiauddin@gmail.com>
2012-04-13 07:12:55 +0000

    - finished with the merc rb

---
+++ application/models/merc.php
@@ -0,0 +1,44 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Merc extends CI_Model 
+	{
+		function __construct()
+			{
+				parent::__construct();
+			}
+#############################################################################################################################
+//CRUD for hstable
+	function merc_getAll($char, $hsid)
+		{
+			return $this->db->get_where('HSDB.dbo.MERC', array('MasterName' => $char, 'HSID' => $hsid));
+		}
+
+//UPDATE
+	function update_rebirth($hsid, $mastername, $rb)
+		{
+			return $this->db->where(array('HSID' => $hsid, 'MasterName' => $mastername))->update('HSDB.dbo.MERC', array('rb' => $rb));
+		}
+
+	
+//INSERT
+	function insert_merc($HSID, $HSName, $MasterName, $Type, $HSLevel)
+		{
+			$array = array
+							(
+								'HSID' => $HSID,
+								'HSName' => $HSName,
+								'MasterName' => $MasterName,
+								'Type' => $Type,
+								'HSLevel' => $HSLevel,
+								'rb' => 0,
+								'reset_rb' => 0,
+							);
+			return $this->db->insert('HSDB.dbo.MERC', $array);
+		}
+
+//DELETE
+
+#############################################################################################################################
+	}
+?>
