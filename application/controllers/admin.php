0c800890db0b79b72f86b368525165c9b7c634ad
dhiauddin <dhiauddin@gmail.com>
2012-04-20 02:46:02 +0000

    - admin news editing

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -5,19 +5,41 @@ class Admin extends CI_Controller
 	{
 #############################################################################################################################
 
-		public function editing_news()
+		public function index()
 			{
 				if ( ($this->session->userdata('logged_in') == TRUE) && ($this->session->userdata('group') == 'GM') )
 					{
 						//process
+						$data['news'] = $this->a3web_html->news();
+						$data['char'] = $this->charac0->charac_char();
 						$this->form_validation->set_error_delimiters('&nbsp;&nbsp;<font color="#FF0000">', '</font>&nbsp;&nbsp;');
 						if ($this->form_validation->run() == FALSE)
 							{
 								//form
+								$this->load->view('admin/home', $data);
 							}
 							else
 							{
 								//form processor
+								if ($this->input->post('add_news'))
+									{
+										$subject = $this->input->post('subject', TRUE);
+										$author = $this->input->post('character', TRUE);
+										$html = stripslashes($this->input->post('news_add', TRUE));
+
+										$j = $this->a3web_html->insert_news($author, $subject, $html);
+										
+										if (!$j)
+											{
+												$data['info'] = 'News not inserted. Please try again';
+												$this->load->view('admin/home', $data);
+											}
+											else
+											{
+												$data['info'] = 'News inserted';
+												$this->load->view('admin/home', $data);
+											}
+									}
 							}
 					}
 					else
@@ -25,6 +47,224 @@ class Admin extends CI_Controller
 						redirect(base_url(), 'location');
 					}
 			}
+
+		public function news_edit()
+			{
+				if ( ($this->session->userdata('logged_in') == TRUE) && ($this->session->userdata('group') == 'GM') )
+					{
+						//process
+						$bil = $this->uri->segment(3, 0);
+						$data['news'] = $this->a3web_html->news_id($bil);
+						$data['char'] = $this->charac0->charac_char();
+						if (is_numeric($bil))
+							{
+								$this->form_validation->set_error_delimiters('&nbsp;&nbsp;<font color="#FF0000">', '</font>&nbsp;&nbsp;');
+								if ($this->form_validation->run() == FALSE)
+									{
+										//form
+										$this->load->view('admin/news_edit', $data);
+									}
+									else
+									{
+										//form processor
+										if ($this->input->post('news_edit_btn'))
+											{
+												$subject = $this->input->post('subject', TRUE);
+												$author = $this->input->post('character', TRUE);
+												$html = stripslashes($this->input->post('news_edit', TRUE));
+		
+												$j = $this->a3web_html->update_news($bil, $html, $subject, $author);
+												
+												if (!$j)
+													{
+														$data['info'] = 'News not update. Please try again';
+														$this->load->view('admin/news_edit', $data);
+													}
+													else
+													{
+														$data['info'] = 'News updated';
+														$this->load->view('admin/news_edit', $data);
+													}
+											}
+									}
+							}
+							else
+							{
+								$data['info'] = 'What are you trying to do?';
+								$this->load->view('admin/news_edit', $data);
+							}
+					}
+					else
+					{
+						redirect(base_url(), 'location');
+					}
+			}
+
+		public function news_del()
+			{
+				if ( ($this->session->userdata('logged_in') == TRUE) && ($this->session->userdata('group') == 'GM') )
+					{
+						//process
+						$bil = $this->uri->segment(3, 0);
+						$data['news'] = $this->a3web_html->news_id($bil);
+						if (is_numeric($bil))
+							{
+								$r = $this->a3web_html->delete_a3web($bil);
+								if ($r)
+									{
+										redirect (site_url('admin/index'), 'location');
+									}
+									else
+									{
+										$data[''] = 'Cant delete the news. Please try again later';
+									}
+							}
+							else
+							{
+								$data['info'] = 'What are you trying to do?';
+								$this->load->view('admin/news_edit', $data);
+							}
+					}
+					else
+					{
+						redirect(base_url(), 'location');
+					}
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #############################################################################################################################
 		public function logout()
